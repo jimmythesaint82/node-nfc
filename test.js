44baea817147daaed8cf3ed742febc65a57defa4
jimmythesaint82 <james@tandy.no>
2019-06-27 18:50:02 +0200

    tag data

--- test.js
+++ test.js
@@ -1,31 +1,32 @@
-var nfc  = require('./index').nfc
-  , util = require('util')
-  , version = nfc.version()
-  , devices = nfc.scan()
-  ;
+const nfc = require('./index').nfc,
+    util = require('util'),
+    version = nfc.version(),
+    devices = nfc.scan();
 
-console.log('version: ' + util.inspect(version, { depth: null }));
-console.log('devices: ' + util.inspect(devices, { depth: null }));
+console.log('version: ' + util.inspect(version, {depth: null}));
+console.log('devices: ' + util.inspect(devices, {depth: null}));
 
 function read(deviceID) {
-  console.log('');
-  var nfcdev = new nfc.NFC();
+    console.log('');
+    var nfcdev = new nfc.NFC();
 
-  nfcdev.on('read', function(tag) {
-    console.log(util.inspect(tag, { depth: null }));
-    //if ((!!tag.data) && (!!tag.offset)) console.log(util.inspect(nfc.parse(tag.data.slice(tag.offset)), { depth: null }));
-    //nfcdev.stop();
-  });
+    nfcdev.on('read', function (tag) {
+        // console.log(util.inspect(tag, {depth: null}));
+        if ((!!tag.data) && (!!tag.offset)) {
+            console.log(util.inspect(nfc.parse(tag.data.slice(tag.offset)), { depth: null }));
+        }
+        //nfcdev.stop();
+    });
 
-  nfcdev.on('error', function(err) {
-    console.log(util.inspect(err, { depth: null }));
-  });
+    nfcdev.on('error', function (err) {
+        console.log(util.inspect(err, {depth: null}));
+    });
 
-  nfcdev.on('stopped', function() {
-    console.log('stopped');
-  });
+    nfcdev.on('stopped', function () {
+        console.log('stopped');
+    });
 
-  console.log(nfcdev.start(deviceID));
+    console.log(nfcdev.start(deviceID));
 }
 
 for (var deviceID in devices) read(deviceID);
