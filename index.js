34af88fb2c4170ef7edfd6ed86f0f902375425df
jimmythesaint82 <james@tandy.no>
2019-06-28 14:18:14 +0200

    clean up

--- index.js
+++ index.js
@@ -1,94 +1,94 @@
-var nfc    = require('bindings')('nfc')
-  , events = require('events')
-  , ndef   = require('ndef')
-  ;
+const nfc = require('bindings')('nfc'),
+    events = require('events'),
+    ndef = require('ndef');
 
-var inherits = function(target, source) {
-    for (var k in source.prototype) {
+const inherits = function (target, source) {
+    for (const k in source.prototype) {
         target.prototype[k] = source.prototype[k];
     }
 };
 inherits(nfc.NFC, events.EventEmitter);
 
-exports.nfc = { version : nfc.version
-              , NFC     : nfc.NFC
-              };
-
-exports.nfc.parse = function(data) {
-  var bytes, i, results, tlv;
-
-  results = [];
-
-  for (i = 0, bytes = data.toJSON().data; i < bytes.length; i += tlv.len) {
-    tlv = { type: bytes[i++] };
-    if ((tlv.type === 0xfe) || (i >= bytes.length)) {
-      results.push(tlv);
-      break;
-    }
-
-    tlv.len = bytes[i++];
-    if (tlv.len === 0xff) {
-      if ((i + 1) >= bytes.length) break;
-      tlv.len = bytes[i++] << 8;
-      tlv.len += bytes[i++];
-    }
-    if ((tlv.len > 0) && ((i + tlv.len) < bytes.length)) tlv.value = bytes.slice(i, i + tlv.len);
-    if ((tlv.type === 0x03) && (!!tlv.value)) tlv.ndef = ndef.decodeMessage(tlv.value);
-    if (!!tlv.value) tlv.value = ndef.util.bytesToHexString(tlv.value);
-    results.push(tlv);
-  }
-
-  return results;
+exports.nfc = {
+    version: nfc.version,
+    NFC: nfc.NFC
 };
 
-exports.nfc.scan = function() {
-  var device, devices, i, info, j, k, kv, mod, mods, prop, props, protocol, results, speeds, v, x;
+exports.nfc.parse = function (data) {
+    let bytes, i, results, tlv;
 
-  results = {};
+    results = [];
 
-  devices = nfc.scan();
-  for (device in devices) if (devices.hasOwnProperty(device)) {
-    props = devices[device].info.split('\n');
-    info = {};
-    for (i = 0; i < props.length; i++) {
-      prop = props[i].trim();
-      if (prop === '') continue;
-
-      x = prop.indexOf(':');
-      if (x < 1) {
-        info[i] = prop;
-        continue;
-      }
-
-      k = prop.substring(0, x);
-      v = prop.substring(x + 1).trim();
-      if (v.indexOf(')') === -1) {
-        info[k] = v;
-        continue;
-      }
-
-      kv = v.split('), ');
-      mods = {};
-      for (j = 0; j < kv.length; j++) {
-        mod = kv[j].trim();
-        if (mod === '') continue;
-
-        x = mod.indexOf(' (');
-        if (x < 1) {
-          mods[j] = mod;
-          continue;
+    for (i = 0, bytes = data.toJSON().data; i < bytes.length; i += tlv.len) {
+        tlv = {type: bytes[i++]};
+        if ((tlv.type === 0xfe) || (i >= bytes.length)) {
+            results.push(tlv);
+            break;
         }
-        protocol = mod.substring(0, x);
-        speeds = mod.substring(x + 2).trim();
-        if (speeds.indexOf(')') === (speeds.length - 1)) speeds = speeds.substring(0, speeds.length - 1);
-        mods[protocol] = speeds.split(', ');
-      }
 
-      info[k] = mods;
+        tlv.len = bytes[i++];
+        if (tlv.len === 0xff) {
+            if ((i + 1) >= bytes.length) break;
+            tlv.len = bytes[i++] << 8;
+            tlv.len += bytes[i++];
+        }
+        if ((tlv.len > 0) && ((i + tlv.len) < bytes.length)) tlv.value = bytes.slice(i, i + tlv.len);
+        if ((tlv.type === 0x03) && (!!tlv.value)) tlv.ndef = ndef.decodeMessage(tlv.value);
+        if (!!tlv.value) tlv.value = ndef.util.bytesToHexString(tlv.value);
+        results.push(tlv);
     }
 
-    results[device] = { name: devices[device].name, info: info };
-  }
+    return results;
+};
 
-  return results;
+exports.nfc.scan = function () {
+    let device, devices, i, info, j, k, kv, mod, mods, prop, props, protocol, results, speeds, v, x;
+
+    results = {};
+
+    devices = nfc.scan();
+    for (device in devices) if (devices.hasOwnProperty(device)) {
+        props = devices[device].info.split('\n');
+        info = {};
+        for (i = 0; i < props.length; i++) {
+            prop = props[i].trim();
+            if (prop === '') continue;
+
+            x = prop.indexOf(':');
+            if (x < 1) {
+                info[i] = prop;
+                continue;
+            }
+
+            k = prop.substring(0, x);
+            v = prop.substring(x + 1).trim();
+            if (v.indexOf(')') === -1) {
+                info[k] = v;
+                continue;
+            }
+
+            kv = v.split('), ');
+            mods = {};
+            for (j = 0; j < kv.length; j++) {
+                mod = kv[j].trim();
+                if (mod === '') continue;
+
+                x = mod.indexOf(' (');
+                if (x < 1) {
+                    mods[j] = mod;
+                    continue;
+                }
+                protocol = mod.substring(0, x);
+                speeds = mod.substring(x + 2).trim();
+                if (speeds.indexOf(')') === (speeds.length - 1)) speeds = speeds.substring(0, speeds.length - 1);
+                mods[protocol] = speeds.split(', ');
+            }
+
+            info[k] = mods;
+        }
+
+        results[device] = {name: devices[device].name, info: info};
+    }
+
+    return results;
 };
--- test.js
+++ test.js
@@ -7,26 +7,27 @@ console.log('version: ' + util.inspect(version, {depth: null}));
 console.log('devices: ' + util.inspect(devices, {depth: null}));
 
 function read(deviceID) {
-    console.log('');
-    var nfcdev = new nfc.NFC();
+    const nfcDev = new nfc.NFC();
 
-    nfcdev.on('read', function (tag) {
+    nfcDev.on('read', function (tag) {
         // console.log(util.inspect(tag, {depth: null}));
         if ((!!tag.data) && (!!tag.offset)) {
-            console.log(util.inspect(nfc.parse(tag.data.slice(tag.offset)), { depth: null }));
+            console.log(util.inspect(nfc.parse(tag.data.slice(tag.offset)), {depth: null}));
         }
-        //nfcdev.stop();
+        // nfcDev.stop();
     });
 
-    nfcdev.on('error', function (err) {
+    nfcDev.on('error', function (err) {
         console.log(util.inspect(err, {depth: null}));
     });
 
-    nfcdev.on('stopped', function () {
+    nfcDev.on('stopped', function () {
         console.log('stopped');
     });
 
-    console.log(nfcdev.start(deviceID));
+    console.log(nfcDev.start(deviceID));
 }
 
-for (var deviceID in devices) read(deviceID);
+for (const deviceID in devices) {
+    read(deviceID);
+}
